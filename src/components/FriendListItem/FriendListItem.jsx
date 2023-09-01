d67a472db441c9b8d4cd436d3826d68870b8c3e8
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-09-01 21:39:43 +0300

    fix friend list

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,11 +5,7 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      {isOnline ? (
-        <span className={css.online}></span>
-      ) : (
-        <span className={css.offline}></span>
-      )}
+      <span className={isOnline ? css.online : css.offline} />
       <img className={css.avatar} src={avatar} alt="User avatar" width="64" />
       <p className={css.name}>{name}</p>
     </li>
